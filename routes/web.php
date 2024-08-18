075f3cbed9c1c0861bbed0108a6939220c4d0518
akprinciple <akeemolayiwola9@gmail.com>
2024-08-18 22:51:11 +0100

    We move

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::redirect('/here', '/welc');
+
+Route::view('/', 'welcome', ['name', 'Akeem']);
+Route::get('/user/{id}/level/{level}', function(string $id, string $level){
+    return $id." ".$level;
 });
