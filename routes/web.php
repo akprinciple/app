2f9a4fe6862c12d29b327ad88d396b1a19a5d725
akprinciple <akeemolayiwola9@gmail.com>
2024-08-19 21:35:39 +0100

    keep moving

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,14 @@ Route::view('/', 'welcome', ['name', 'Akeem']);
 Route::get('/user/{id}/level/{level}', function(string $id, string $level){
     return $id." ".$level;
 });
+Route::get('/post/{postId}/category/{catId?}', function (string $postId, ?string $catId = "None") {
+    return 'Post id is'. $postId." Category given is ".$catId;
+})->where('catId', '[A-Za-z]+');;
+
+Route::get('/user/profile', function () {
+    return "Gotten";
+})->name('profile');
+
+Route::fallback(function () {
+   return 'Route not found!php';
+});
